820b158ea54bf6fa07abd99fcb2f1f97d7fb626b
Psyclone <payu.su.59@ubu.ac.th>
2017-07-11 16:18:53 +0700

    proje

---
+++ app/src/main/java/com/artid/then/Aplash_Activity.java
@@ -0,0 +1,33 @@
+package com.artid.then;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Aplash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aplash_);
+
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+
+
+
+}
--- app/src/main/java/com/artid/then/MainActivity.java
+++ app/src/main/java/com/artid/then/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -18,7 +21,7 @@ import com.google.cloud.translate.Translation;
 
 public class MainActivity extends AppCompatActivity {
     private static final String API_KEY = "AIzaSyD6Rm3n8Ygq6wygh2515sqiRo60D1i24kc";
-    String targetLanguage = "en";
+    String targetLanguage ;
     Handler textViewHandler;
     TextView textView;
     ProgressDialog progressDialog;
@@ -27,14 +30,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+        //final ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
         final EditText editText = (EditText) findViewById(R.id.editText);
         final Button button = (Button) findViewById(R.id.button);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        final String[] language = getResources().getStringArray(R.array.language);
+        ArrayAdapter<String> adeptergade = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, language);
+        spinner.setAdapter(adeptergade);
+
         textView = (TextView) findViewById(R.id.textView);
         textViewHandler = new Handler();
 
 
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        /*toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
@@ -43,7 +53,58 @@ public class MainActivity extends AppCompatActivity {
                     targetLanguage = "en";
                 }
             }
+        });*/
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                 String Language = parent.getItemAtPosition(position).toString();
+               if( Language.equals("Thai")) {
+                    targetLanguage = "th";
+               }
+               if( Language.equals("English")) {
+                    targetLanguage = "en";
+                }
+                if( Language.equals("Japanese")) {
+                    targetLanguage = "ja";
+                }
+                if( Language.equals("Chinese")) {
+                    targetLanguage = "zh-CN";
+                }
+                if( Language.equals("Spanish")) {
+                    targetLanguage = "es";
+                }
+                if( Language.equals("German")) {
+                    targetLanguage = "de";
+                }
+                if( Language.equals("French")) {
+                    targetLanguage = "fr";
+                }
+                if( Language.equals("Dutch")) {
+                    targetLanguage = "nl";
+                }
+                if( Language.equals("Croatian")) {
+                    targetLanguage = "hr";
+                }
+                if( Language.equals("Italian")) {
+                    targetLanguage = "it";
+                }
+
+
+            } // to close the onItemSelected
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+
+            }
         });
+
+       /* button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TranslateText().execute(editText.getText().toString());
+            }
+        });*/
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
